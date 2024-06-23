37056d024964c5ac5637893879952227c8c6912d
Aakash Dasgupta <dasgupta_aakash@ymail.com>
2024-06-23 16:30:11 -0500

    changed formatting and added developers module

--- client/src/components/ProjectView/ProjectView.js
+++ client/src/components/ProjectView/ProjectView.js
@@ -20,16 +20,17 @@ export default function ProjectView() {
                 <h2>Summary</h2>
             </div>
             <div className={styles["project__summary"]}>
-                <div className={styles["project__summary--manager"]}>
-                    <img className={styles["profile__img"]} src={profile} alt="Manager"/>
-                    <h3>{project.manager.name}</h3>
+                <div className={styles["project__details--graph"]}>
+                    <h2>Progress</h2>
+                    <img className={styles["graph__img"]} src={pie} alt="Pie" />
+                    <p><strong>Completion:</strong> 50%</p>
                 </div>
                 <div className={styles["project__summary--info"]}>
-                    <p><strong>Description:</strong> <br />{project.longDescription}</p>
+                    <p className={styles["project__description"]}><strong>Description:</strong> <br />{project.longDescription}<br /><br /> <strong>Manager:</strong> {project.manager.name}</p>
                     <div className={styles["project__summary--critical"]}>
-                        <p>Priority: <br /> {project.priority}</p>
-                        <p>Status:<br />{project.status}</p>
-                        <p>Deadine: <br /> {project.deadline}</p>
+                        <p><strong>Priority:</strong> <br /> {project.priority}</p>
+                        <p><strong>Status: </strong><br />{project.status}</p>
+                        <p><strong>Deadline: </strong> <br /> {project.deadline}</p>
                     </div>
                 </div>
             </div>
@@ -37,10 +38,21 @@ export default function ProjectView() {
                 <h2>Details</h2>
             </div>
             <div className={styles["project__details"]}>
-                <div className={styles["project__details--graph"]}>
-                    <h2>Progress</h2>
-                    <img className={styles["graph__img"]} src={pie} alt="Pie" />
-                    <p><strong>Completion:</strong> 50%</p>
+                {/* <div className={styles["project__summary--manager"]}>
+                    <img className={styles["profile__img"]} src={profile} alt="Manager"/>
+                    <h3>{project.manager.name}</h3>
+                </div> */}
+                <div className={styles["project__details--developers"]}>
+                    <h2>Developers</h2>
+                    {
+                        project.assignedDevs.map((dev) => {
+                            return (
+                                <div>
+                                    <p>{dev.name}</p>
+                                </div>
+                            )
+                        })
+                    }
                 </div>
                 <div className={styles["project__details--tickets"]}>
                     <h2>Tickets</h2>
@@ -51,10 +63,10 @@ export default function ProjectView() {
                                     <div>
                                         <h4>{ticket.title}</h4>
                                         <div className={styles["ticket__info"]}>
-                                            <p className="small-text">Deadline : {ticket.deadline}</p>
-                                            <p className="small-text">Priority : {ticket.deadline}</p>
-                                            <p className="small-text">Status : {ticket.status ? ticket.status : "Unassigned"}</p>
-                                            <p className="small-text">Assigned to: {ticket.assignedDev.name}</p>
+                                            <p className="small-text"><strong>Deadline :</strong> {ticket.deadline}</p>
+                                            <p className="small-text"><strong>Priority :</strong> {ticket.deadline}</p>
+                                            <p className="small-text"><strong>Status : </strong>{ticket.status ? ticket.status : "Unassigned"}</p>
+                                            <p className="small-text"><strong>Assigned to:</strong> {ticket.assignedDev.name}</p>
                                         </div>
                                     </div>
                                     <hr />
