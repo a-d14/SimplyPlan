ea6b0506f28182e6649969a07f5c08aab217a097
Aakash Dasgupta <dasgupta_aakash@ymail.com>
2024-06-24 08:29:12 -0500

    added styling to sidebar

--- client/src/components/Sidebar/Sidebar.js
+++ client/src/components/Sidebar/Sidebar.js
@@ -1,32 +1,26 @@
 import { NavLink } from "react-router-dom";
 import styles from './Sidebar.module.css';
 
+import profile from '../../images/profile-img.png';
+
 export default function Sidebar() {
     return (
         <div className={styles.sidebar}>
             <div className={styles['sidebar-header']}>
                 <h1>SimplyPlan</h1>
+                <div className={styles['user-profile']}>
+                    <img className={styles['user-profile__image']} src={profile} alt="Profile" />
+                    <p className={styles['user-profile__user-name']}>Current User (<NavLink to="">Open Profile</NavLink>)</p>
+                </div>
             </div>
             <div className={styles['sidebar-nav']}>
-                <nav>
-                    <ul className={styles['nav-links']}>
-                        <li>
-                            <NavLink to=''>Home</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to='tickets'>Tickets</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to='projects'>Projects</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to='notes'>Notes</NavLink>
-                        </li>
-                    </ul>
-                </nav>
+                <NavLink className={styles['sidebar-nav__links']} to=''>Home</NavLink>
+                <NavLink className={styles['sidebar-nav__links']} to='tickets'>Tickets</NavLink>
+                <NavLink className={styles['sidebar-nav__links']} to='projects'>Projects</NavLink>
+                <NavLink className={styles['sidebar-nav__links']} to='notes'>Notes</NavLink>
             </div>
             <div className={styles['sidebar-footer']}>
-                <button>Logout</button>
+                <NavLink to="">Logout</NavLink>
             </div>
         </div>
     )
